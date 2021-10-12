fe76069c753abcd20c77631a209104eaecd7e411
Laura Pearlman <laura@isi.edu>
2021-10-12 05:46:22 -0700

    packaging tweaks

--- preprocessors/setup.py
+++ preprocessors/setup.py
@@ -5,6 +5,6 @@ setup(
     description='3d visualization tools',
     version="0.1",
     packages=['viz_3d', 'viz_3d.volumes'],
-    install_requires=['deriva', 'vtk', 'pyunpack'],
+    install_requires=['deriva', 'vtk', 'pyunpack', 'aiohttp<4.0.0'],
     scripts=['bin/process-3d-images', 'bin/count-voxels']
 )
